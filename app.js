ef5ff3eeada5278bb54e19eea0b7e05dc2871477
sumansingh6degreeit <99945059+sumansingh6degreeit@users.noreply.github.com>
2023-08-01 02:36:45 +0530

    hello world usign html css react

---
+++ app.js
@@ -0,0 +1,4 @@
+
+const heading = React.createElement("h1", { id: "heading", xyz: "zyz" }, "Hello world!");
+const root = ReactDOM.createRoot(document.getElementById('root'));
+root.render(heading);
