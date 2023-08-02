25b7340891fa09d4a3df8883e8eb7cbb5bab03d0
sumansingh6degreeit <99945059+sumansingh6degreeit@users.noreply.github.com>
2023-08-03 01:49:40 +0530

    add multiplr child

--- app.js
+++ app.js
@@ -1,4 +1,20 @@
+const heading = React.createElement(
+    "h1",
+    { id: "heading", xyz: "zyz" },
+    "Hello world!"
+);
 
-const heading = React.createElement("h1", { id: "heading", xyz: "zyz" }, "Hello world!");
-const root = ReactDOM.createRoot(document.getElementById('root'));
-root.render(heading);
+const parent = React.createElement(
+    "div",
+    { id: "parent" }, [
+    React.createElement("div", { id: "child" }, [
+        React.createElement("h1", { id: "heading", key: 0 }, "heading1.1 child"),
+        React.createElement("h1", { id: "heading", key: 1 }, "heading1.2 child"),
+    ]),
+    React.createElement("div", { id: "child" }, [
+        React.createElement("h1", { id: "heading", key: 0 }, "heading2.1 child"),
+        React.createElement("h1", { id: "heading", key: 1 }, "heading2.1 child"),
+    ])]
+);
+const root = ReactDOM.createRoot(document.getElementById("root"));
+root.render(parent);
