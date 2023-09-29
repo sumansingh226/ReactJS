d96eadaabcaabeb48f84daac6ee070731924c0c2
Suman Singh <99945059+sumansingh6degreeit@users.noreply.github.com>
2023-09-30 03:40:23 +0530

    implement search functionality

--- src/Components/Body.jsx
+++ src/Components/Body.jsx
@@ -5,6 +5,11 @@ import Simmer from "./Simmer";
 
 const Body = () => {
   const [restorentsList, setRestorentsList] = useState([]);
+  const [
+    searchedFilteredRestorentsListList,
+    setSearchedFilteredRestorentsListList,
+  ] = useState([]);
+  const [searchInputValue, setSearchInputValue] = useState("");
 
   useEffect(() => {
     fetchRestoretData();
@@ -13,6 +18,7 @@ const Body = () => {
   const fetchRestoretData = async () => {
     let clearTimer = setTimeout(() => {
       setRestorentsList([...restorentsListData]);
+      setSearchedFilteredRestorentsListList([...restorentsListData]);
       clearTimeout(clearTimer);
     }, 3000);
   };
@@ -21,21 +27,57 @@ const Body = () => {
     let filterTopRatedRes = restorentsList.filter(
       (restorent) => restorent.info.avgRating >= 4
     );
-    setRestorentsList([...filterTopRatedRes]);
+    setSearchedFilteredRestorentsListList([...filterTopRatedRes]);
+  };
+
+  const handleSearchInputValue = (event) => {
+    setSearchInputValue(event.target.value);
+  };
+
+  const filterSearchRestorents = () => {
+    const searchedRestorents = restorentsList.filter((restro) =>
+      restro.info.name
+        ?.toString()
+        ?.toLocaleLowerCase()
+        ?.includes(searchInputValue?.toLocaleLowerCase())
+    );
+    setSearchedFilteredRestorentsListList([...searchedRestorents]);
+  };
+
+  const RestorentsNotFound = () => {
+    return (
+      <div className="empty-restorent-container">
+        <div className="empty-restorent-box"> No restrents found </div>
+      </div>
+    );
   };
 
   return (
     <div>
       <div className="filter">
+        <div className="serach">
+          <input
+            className="searc-input.input"
+            placeholder="Searc restorents"
+            type="text"
+            value={searchInputValue}
+            onChange={handleSearchInputValue}
+          />
+          <button className="searcBtn" onClick={filterSearchRestorents}>
+            search
+          </button>
+        </div>
         <button className="filter-btn" onClick={handleFilterTopRatedRestorents}>
           Top Rated Restorents
         </button>
       </div>
       {restorentsList.length === 0 ? (
         <Simmer />
+      ) : searchedFilteredRestorentsListList.length === 0 ? (
+        <RestorentsNotFound />
       ) : (
         <div className="restorent-container">
-          {restorentsList?.map((restorent) => {
+          {searchedFilteredRestorentsListList?.map((restorent) => {
             return (
               <RestroRentCard
                 key={restorent.info.id}
