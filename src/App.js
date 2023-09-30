154ef0e7d3c3806ef42dbe2c34e82fd857daaf96
Suman Singh <99945059+sumansingh6degreeit@users.noreply.github.com>
2023-09-30 18:25:19 +0530

    react-router-dom and error page for 404

--- src/App.js
+++ src/App.js
@@ -2,6 +2,10 @@ import React from "react";
 import ReactDOM from "react-dom/client";
 import Body from "./Components/Body";
 import Header from "./Components/Header";
+import { createBrowserRouter, RouterProvider } from "react-router-dom";
+import About from "./Components/About";
+import Contact from "./Components/Contact";
+import Error from "./Components/Error";
 
 const AppLayout = () => {
     return (
@@ -11,6 +15,23 @@ const AppLayout = () => {
         </div>
     );
 };
+const appRouter = createBrowserRouter([
+    {
+        path: "/",
+        element: <AppLayout />,
+        errorElement: <Error />
+    },
+    {
+        path: "/about",
+        element: <About />
+    }
+    ,
+    {
+        path: "/contact",
+        element: <Contact />
+    }
+])
 
 const root = ReactDOM.createRoot(document.getElementById("root"));
-root.render(<AppLayout />);
+root.render(<RouterProvider router={appRouter} />);
+
---
+++ src/Components/Error.jsx
@@ -0,0 +1,12 @@
+import React from "react";
+
+const Error = () => {
+  return (
+    <div>
+      <h1>Error:404</h1>
+      <p>oops something went wrong.</p>
+    </div>
+  );
+};
+
+export default Error;
