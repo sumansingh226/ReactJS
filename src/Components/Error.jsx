41678f84c0ccf4e546b1f540a8ba091ec82225a6
Suman Singh <99945059+sumansingh6degreeit@users.noreply.github.com>
2023-09-30 18:33:46 +0530

    react-router-dom error

--- src/Components/Error.jsx
+++ src/Components/Error.jsx
@@ -1,10 +1,13 @@
 import React from "react";
+import { useRouteError } from "react-router-dom";
 
 const Error = () => {
+  const err = useRouteError();
   return (
     <div>
       <h1>Error:404</h1>
-      <p>oops something went wrong.</p>
+      <h3>oops something went wrong.</h3>
+      <h4>{err.status}</h4>
     </div>
   );
 };
